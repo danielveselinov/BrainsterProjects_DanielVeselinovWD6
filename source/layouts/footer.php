f4799772742d95d4ef53301f245b3c7e76edb755
Daniel Veselinov <daniel@veselinov.com>
2022-05-28 16:50:06 +0200

    Required components

---
+++ source/layouts/footer.php
@@ -0,0 +1,25 @@
+<footer class="bg-dark text-light text-center">
+    <div class="container py-3">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-md-12 col-xl-6">
+                <div class="card text-light bg-secondary rounded">
+                    <div class="card-body p-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-quote" viewBox="0 0 16 16">
+                            <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z" />
+                        </svg>
+                        <p class="lead">Genius is one percent inspiration and ninety-nine percent perspiration.</p>
+                        <hr>
+                        <div class="d-flex justify-content-between">
+                            <p class="mb-0">Thomas Edison</p>
+                            <h6 class="mb-0"><span class="badge rounded-pill" style="background-color: rgba(0,0,0, 0.2);">876</span> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill ms-2" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+                                </svg>
+                            </h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <p class="fw-bold p-3 mb-0 border-top">Made with <span class="text-danger">&hearts;</span> from Brainster Students</p>
+</footer>
