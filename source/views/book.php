1935550630a5f3c9a7a34e2c31889baf81d54322
Daniel Veselinov <daniel@veselinov.com>
2022-06-03 17:37:33 +0200

    Implementing notes functionality and logic

--- source/views/book.php
+++ source/views/book.php
@@ -42,7 +42,7 @@ require_once __DIR__ . "/../layouts/navbar.php"; ?>
                 <?php $stmt = DB::connect()->query("SELECT comments.*, users.id as user_id, users.fullname, users.is_admin FROM comments
                 JOIN `users` ON `comments`.`existing_user_id` = users.id 
                 WHERE 1");
-
+                // MORA: ako vekje ima komentar da ne moze da komentira pak..
                 if ($stmt->rowCount() > 0) {
                     while ($commentsData = $stmt->fetch()) {
                         if ($bookData['id'] == $commentsData['commented_on_book']) {
@@ -68,10 +68,12 @@ require_once __DIR__ . "/../layouts/navbar.php"; ?>
                                     <p class="text-muted small mb-0"><?= $commentsData['created_at'] ?></p>
                                     <p class="mt-3 mb-0"><?= $commentsData['comment'] ?></p>
                                 </div>
-                            <?php }
-                        } 
-                    } 
-                 } else { echo "<p class='ms-3 mt-3'>There aren't any comments yet</p>"; } ?>
+                <?php }
+                        }
+                    }
+                } else {
+                    echo "<p class='ms-3 mt-3'>There aren't any comments yet</p>";
+                } ?>
 
                 <?php if (Auth::isLogged()) { ?>
                     <form class="card-footer">
@@ -89,6 +91,52 @@ require_once __DIR__ . "/../layouts/navbar.php"; ?>
                     </div>
                 <?php } ?>
             </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row pb-5 justify-content-center">
+        <div class="col-md-10 col-xl-8">
+            <?php
+            if (Auth::isLogged()) { ?>
+                <div class="d-flex align-items-center mb-2">
+                    <p class="fw-bold small text-uppercase my-auto me-2">private notes</p>
+                    <button id='createMyNoteAction' class='btn btn-outline-dark btn-sm text-uppercase'>Create note</button>       
+                </div>
+           <?php } 
+            $stmt = DB::connect()->query("SELECT `notes`.*, `users`.`id` as user_id, `books`.`id` as book_id FROM notes
+            JOIN `users` ON `notes`.`existing_user_id` = `users`.`id`
+            JOIN `books` ON `notes`.`note_on_book` = `books`.`id`
+            WHERE `notes`.`note_on_book` = {$bookData['id']} ORDER BY created_at DESC");
+
+            if ($stmt->rowCount() > 0) {
+                while ($notesData = $stmt->fetch()) { 
+                    if (Auth::isLogged() && $notesData['user_id'] == Auth::id()) { ?>
+                    <div class="accordion mt-2" id="accordion<?= $notesData['id'] ?>">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="heading<?= $notesData['id'] ?>">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?= $notesData['id'] ?>">
+                                    Note #{<?= $notesData['id'] ?>} - <?= $notesData['created_at'] ?>
+                                </button>
+                            </h2>
+                            <div id="collapse<?= $notesData['id'] ?>" class="accordion-collapse collapse" data-bs-parent="#accordion<?= $notesData['id'] ?>">
+                                <div class="accordion-body">
+                                    <div class="form-floating">
+                                        <div class="form-floating">
+                                            <textarea class="form-control" placeholder="Leave a note here" id="noteContent" style="height: 100px; resize: none;"><?= $notesData['note_text'] ?></textarea>
+                                            <label for="noteContent">Leave a note here</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-footer">
+                                    <button id='updateMyNoteAction' data-note-id='<?= $notesData['id'] ?>' class='btn btn-outline-dark btn-sm'>Update note</button>
+                                    <button id='deleteMyNoteAction' data-note-id='<?= $notesData['id'] ?>' class='btn btn-danger btn-sm'>Delete note</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+              <?php } } } ?>
         </div>
     </div>
 </div>
