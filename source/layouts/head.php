5e37e0facac22574d22794902d8473156139c896
Daniel Veselinov <daniel@veselinov.com>
2022-06-26 23:07:21 +0200

    Update head.php & SEO Optimization

--- source/layouts/head.php
+++ source/layouts/head.php
@@ -1,8 +1,21 @@
     <title>Brainster Library</title>
     <meta charset="utf-8" />
-    <meta name="keywords" content="Brainster Library, 2nd project, Brainster academy, coding, development, backend" />
-    <meta name="description" content="Second project during my FS Academy at Brainster" />
-    <meta name="author" content="Daniel Veselinov (github.com/danielveselinov)" />
+
+    <meta name="title" content="Brainster Library - Online library management system">
+    <meta name="description" content="Second project during my FS Academy at Brainster">        
+    <meta name="image" content="https://codepreneur.mk/brainsterlibrary/source/assets/images/banner-1.jpg">
+    <meta name="author" content="Daniel Veselinov ( https://codepreneur.mk/ )">
+    <meta property='og:site_name' content='Brainster Library - Online library management system'/>
+    <meta property='og:url' content='https://codepreneur.mk/'/>
+    <meta property='og:title' content='Brainster Library - Online library management system'/>
+    <meta property='og:keywords' content="brainster library, project, brainster academy, coding, development, backend, full stack development, php, laravel, ajax, jquery" />
+    <meta property='og:type' content='article'/>
+    <meta property="og:image" content="https://codepreneur.mk/brainsterlibrary/source/assets/images/banner-1.jpg" />
+    <meta property="og:description" content="Second project during my FS Academy at Brainster" />
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="Brainster Library - Online library management system">
+    <meta name="twitter:description" content="Second project during my FS Academy at Brainster">
+    <meta name="twitter:image" content="https://codepreneur.mk/brainsterlibrary/source/assets/images/banner-1.jpg">
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
 
     <link rel="icon" type="image/png" sizes="32x32" href="https://realfavicongenerator.net/the_favicon/favicon-32x32.png?v=zX7n49rwEM">
